4ae3c71f3169fac73e2a0598d807d7d70e361296
mary <laguta113@gmail.com>
2023-11-21 18:34:39 +0600

    Commit with a solution

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,18 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        DigitsSumCalculator calculator = new DigitsSumCalculator();
+
+        calculator.calculateSum(1111);
+        calculator.calculateSum(4567);
     }
 }
